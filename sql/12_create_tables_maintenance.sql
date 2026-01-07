29d76f00dccc53aa583b108841c4ee05367e13bd
Altreon117 <rahp13450@gmail.com>
2026-01-07 20:24:20 +0100

    création de la documentation (mcd et mld aussi mis en solo), ajout des queries de Matteo et correction de petites erreurs de syntaxe

--- sql/12_create_tables_maintenance.sql
+++ sql/12_create_tables_maintenance.sql
@@ -11,7 +11,7 @@ CREATE TABLE TECHNICIEN (
 
 CREATE TABLE PANNE (
     id_panne SERIAL PRIMARY KEY,
-    description TEXT,
+    "description" TEXT,
     date_signalement DATE DEFAULT CURRENT_DATE,
     severite VARCHAR(20) CHECK (severite IN ('FAIBLE', 'MOYENNE', 'CRITIQUE')),
     id_vehicule INT REFERENCES VEHICULE(id)
--- sql/99_queries.sql
+++ sql/99_queries.sql
@@ -1,10 +1,38 @@
 --##### PARTIE MATTEO ###############################################
 
+-- 1. Répartition de la flotte par type de véhicule
+SELECT t.libelle AS type_vehicule, COUNT(v.id) as nombre_vehicules
+FROM type_vehicule t
+LEFT JOIN vehicule v ON t.id = v.id_type
+GROUP BY t.libelle;
 
+-- 2. Top 3 des stations avec le plus de véhicules disponibles
+SELECT s.nom AS station_nom, COUNT(v.id) as nb_disponibles
+FROM station s
+JOIN vehicule v ON s.id = v.id_station
+WHERE v.statut = 'DISPONIBLE'
+GROUP BY s.nom
+ORDER BY nb_disponibles DESC
+LIMIT 3;
+
+-- 3. Moyenne de batterie par type de véhicule (pour les véhicules électriques)
+SELECT t.libelle AS type_vehicule, ROUND(AVG(v.niveau_batterie), 1) as batterie_moyenne
+FROM vehicule v
+JOIN type_vehicule t ON v.id_type = t.id
+WHERE t.libelle LIKE '%Électrique%'
+GROUP BY t.libelle;
+
+-- 4. Taux d'occupation des stations (en %)
+SELECT nom AS station_nom, capacite, 
+       ROUND((SELECT COUNT(*) FROM vehicule WHERE id_station = s.id)::DECIMAL / NULLIF(capacite,0) * 100, 1) as taux_occupation
+FROM station s
+ORDER BY capacite DESC
+LIMIT 50;
 
 
 --##### PARTIE THIBAUD ###############################################
 
+-- 1. Calculer le prix total d'une réservation en fonction de la durée
 CREATE OR REPLACE FUNCTION calculer_prix_course(p_id_reservation INT, p_prix_minute DECIMAL) 
 RETURNS DECIMAL AS $$
 DECLARE
@@ -26,10 +54,12 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- 2. Liste des réservations terminées avec leur prix total
 SELECT id_reservation, statut, calculer_prix_course(id_reservation, 0.25) AS prix_final
-FROM reservation
+FROM reservation    
 WHERE statut = 'TERMINEE';
 
+-- 3. Top 3 des clients les plus fidèles (nombre de trajets)
 SELECT c.nom, c.prenom, COUNT(r.id_reservation) as total_trajets
 FROM client c
 JOIN reservation r ON c.id_client = r.id_client
